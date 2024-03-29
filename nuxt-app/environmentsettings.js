6b5aaca776be8f276504efde6470525b58a3cd61
John <john.norton.ca@gmail.com>
2024-03-29 15:47:43 -0700

    changed client id

--- nuxt-app/environmentsettings.js
+++ nuxt-app/environmentsettings.js
@@ -20,7 +20,7 @@ const GlobalSettings = {
   production: {
     siteEnvironment: '',
     telephone: '333 333-3333',
-    MSAL_CLIENT_ID: 'c5928d79-4d39-47c0-8f38-da6a3beae57b',
+    MSAL_CLIENT_ID: 'd2107079-5f9e-4f3f-8b38-f87b4dd9ea04',
     MSAL_AUTHORITY:
       'https://nomososo.b2clogin.com/nomososo.onmicrosoft.com/B2C_1_Sign_Up_And_Sign_In',
     MSAL_KNOWN_AUTHORITIES: ['nomososo.b2clogin.com'],
