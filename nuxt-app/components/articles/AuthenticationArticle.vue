d699184195ddadfb6efe98e6587b83510003683b
John <john.norton.ca@gmail.com>
2024-04-23 11:36:52 -0700

    Beginning of Authentication Article (hard coded)
    Will use a data driven approach when I figure out what I need

---
+++ nuxt-app/components/articles/AuthenticationArticle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="flex-parent-element">
+      <div class="flex-child-element menu content">
+        <h1>Authentication</h1>
+        <p>Choosing the right technology</p>
+      </div>
+
+      <div class="flex-child-element context content">
+        <h2>Needs based selection</h2>
+        <p>
+          When the time came to choose how I wanted to authenticate users I
+          wanted to give the choice of creating a username and password or using
+          a social account like Google so they wouldn’t have to remember yet
+          another password.
+        </p>
+        <p>
+          The needs of this site are humble. The site is a Business to Consumer
+          (B2C) application for a single tenant. People need to be able to sign
+          up, sign in, and reset their password without support. The user flow
+          should be simple and familiar.
+        </p>
+        <p>
+          The system is built on Microsoft Azure Cloud technology. The two
+          finalists were Microsoft Entra External ID and Microsoft Azure Active
+          Directory B2C (AD B2C).
+        </p>
+        <p>
+          Microsoft positions Entra External ID as the next stage evolution to
+          seamlessly unite secure and engaging experiences for a wide range of
+          external identities including customers and partners as citizens
+          within a single cohesive platform. Whereas AD B2C is basically used to
+          build customer facing applications only.
+        </p>
+        <p>
+          I like the vision of Entra External ID. That’s been my mental model of
+          how identities work. All excitement aside, the AD B2C does what I need
+          today and is inexpensive under the pay as you go model. The first
+          50,000 monthly active users (MAU) are free.
+        </p>
+        <p>
+          I chose AD B2C because it is a mature product that does what I need,
+          is easy to use and has a lot of documentation. I can always switch to
+          Entra External ID later if I need to.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- nuxt-app/pages/main-page.vue
+++ nuxt-app/pages/main-page.vue
@@ -1,97 +1,6 @@
 <template>
   <div class="page">
-    <div class="page-header">
-      <h1>Main page</h1>
-    </div>
-    <div class="flex-parent-element">
-      <div class="flex-child-element menu content">
-        <h1>Authentication</h1>
-        <p>Microsoft Azure AD B2C</p>
-      </div>
-
-      <div class="flex-child-element context content">
-        <h2>Authentication using Microsoft Azure Active Directory B2C</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Viverra nam libero justo laoreet sit amet. Est pellentesque elit
-          ullamcorper dignissim cras tincidunt lobortis feugiat. Facilisis magna
-          etiam tempor orci. Id semper risus in hendrerit gravida rutrum quisque
-          non. Diam vulputate ut pharetra sit amet aliquam id diam. Varius quam
-          quisque id diam vel quam elementum. Tellus orci ac auctor augue mauris
-          augue neque. In massa tempor nec feugiat. A erat nam at lectus urna
-          duis. Consectetur libero id faucibus nisl tincidunt eget nullam. Urna
-          condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-          Etiam non quam lacus suspendisse faucibus interdum. Eu ultrices vitae
-          auctor eu augue. Tellus cras adipiscing enim eu turpis egestas
-          pretium. Morbi tristique senectus et netus et malesuada fames ac. Amet
-          porttitor eget dolor morbi non arcu risus. Consectetur lorem donec
-          massa sapien.
-        </p>
-        <p>
-          Non blandit massa enim nec dui nunc mattis enim ut. Tristique nulla
-          aliquet enim tortor. Pharetra massa massa ultricies mi. At lectus urna
-          duis convallis convallis tellus. Sed euismod nisi porta lorem mollis
-          aliquam ut. Maecenas pharetra convallis posuere morbi leo urna. Et leo
-          duis ut diam quam nulla. In ornare quam viverra orci sagittis eu
-          volutpat odio. Platea dictumst quisque sagittis purus sit amet. Vel
-          risus commodo viverra maecenas accumsan. Habitant morbi tristique
-          senectus et netus et.
-        </p>
-      </div>
-    </div>
-
-    <div class="flex-parent-element">
-      <div class="flex-child-element menu">
-        <h1>left left left left left left left left left</h1>
-        <p>My main page content My main page content My main page content</p>
-      </div>
-
-      <div class="flex-child-element context">
-        <h2>Right Right Right Right Right Right Right Right</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Viverra nam libero justo laoreet sit amet. Est pellentesque elit
-          ullamcorper dignissim cras tincidunt lobortis feugiat. Facilisis magna
-          etiam tempor orci. Id semper risus in hendrerit gravida rutrum quisque
-          non. Diam vulputate ut pharetra sit amet aliquam id diam. Varius quam
-          quisque id diam vel quam elementum. Tellus orci ac auctor augue mauris
-          augue neque. In massa tempor nec feugiat. A erat nam at lectus urna
-          duis. Consectetur libero id faucibus nisl tincidunt eget nullam. Urna
-          condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-          Etiam non quam lacus suspendisse faucibus interdum. Eu ultrices vitae
-          auctor eu augue. Tellus cras adipiscing enim eu turpis egestas
-          pretium. Morbi tristique senectus et netus et malesuada fames ac. Amet
-          porttitor eget dolor morbi non arcu risus. Consectetur lorem donec
-          massa sapien.
-        </p>
-        <p>
-          Non blandit massa enim nec dui nunc mattis enim ut. Tristique nulla
-          aliquet enim tortor. Pharetra massa massa ultricies mi. At lectus urna
-          duis convallis convallis tellus. Sed euismod nisi porta lorem mollis
-          aliquam ut. Maecenas pharetra convallis posuere morbi leo urna. Et leo
-          duis ut diam quam nulla. In ornare quam viverra orci sagittis eu
-          volutpat odio. Platea dictumst quisque sagittis purus sit amet. Vel
-          risus commodo viverra maecenas accumsan. Habitant morbi tristique
-          senectus et netus et.
-        </p>
-      </div>
-    </div>
+    <ArticlesAuthenticationArticle />
   </div>
 </template>
 
@@ -106,7 +15,7 @@ definePageMeta({
 })
 </script>
 
-<style scoped>
+<style>
 /* Styles for the h1 elements */
 h1 {
   color: var(--clr-primary);
